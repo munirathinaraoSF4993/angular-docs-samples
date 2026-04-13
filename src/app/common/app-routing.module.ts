cbe6810a9cf044b4453209d843042ec64fa64c49
munirathinaraoSF4993 <munirathinarao.munusamy@syncfusion.com>
2026-04-13 13:26:17 +0530

    feature(REPORT-502080): Removed the unwanted changes

--- src/app/common/app-routing.module.ts
+++ src/app/common/app-routing.module.ts
@@ -4,17 +4,20 @@ import { BoldReportsModule } from '@boldreports/angular-reporting-components';
 import { Routes, RouterModule } from '@angular/router';
 import { ReportViewerGettingStartedComponent } from '../components/report-viewer/getting-started/getting-started.component';
 import { ReportDesignerGettingStartedComponent } from '../components/report-designer/getting-started/getting-started.component';
-const components: any[] | Type<any> | ModuleWithProviders<{}> = [
+
+const components: any[] | Type<any> | ModuleWithProviders<{}> = [
   ReportViewerGettingStartedComponent,
   ReportDesignerGettingStartedComponent
 ];
-const routes: Routes = [
+
+const routes: Routes = [
   { path: '', redirectTo: 'report-viewer/getting-started', pathMatch: 'full' },
   { path: 'report-viewer/getting-started', component: ReportViewerGettingStartedComponent },
   { path: 'report-designer/getting-started', component: ReportDesignerGettingStartedComponent },
   { path: '**', redirectTo: 'report-viewer/getting-started' }
 ];
-@NgModule({
+
+@NgModule({
   declarations: components,
   imports: [
     BrowserModule,
